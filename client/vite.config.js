51a99234dba2215791bcd7e457902d152d763769
Pariharx7 <parihar.peace@gmail.com>
2025-09-04 09:56:42 +0530

    minor ui fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': import.meta.env.VITE_BACKEND_URL   // modify this to backend link in production
+      '/api': "https://gemx-backend.vercel.app"   // modify this to backend link in production
     },
   },
   plugins: [
