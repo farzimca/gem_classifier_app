8eb7e6c0fba1b66cc67db1498f6339bccd3c207f
farzimca <webdevlrn@gmail.com>
2025-09-29 22:48:20 +0530

    Revert "Update db.js"

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,38 +1,18 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+const dbconnect = async() =>
+{
+
+    try {
+      const dbconnectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`DB Connected : DB HOST = ${dbconnectInstance.connection.host}`)
+    } catch (error) {
+        console.log("Error in DB Connection", error)
+        process.exit(1)
+    }
 }
 
-async function dbconnect() {
-  if (cached.conn) {
-    // Reuse existing connection in Vercel or Express
-    console.log("=> using existing database connection");
-    return cached.conn;
-  }
 
-  if (!cached.promise) {
-    const uri = `${process.env.MONGODB_URI}/${DB_NAME}`;
-
-    cached.promise = mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 5000, // fail fast if DB isn't reachable
-      bufferCommands: false,          // better for serverless
-    })
-    .then((mongoose) => {
-      console.log("=> new database connection created");
-      return mongoose;
-    })
-    .catch((err) => {
-      console.error("❌ Error in DB Connection:", err.message);
-      throw err;
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
-export default dbconnect;
+export default dbconnect
