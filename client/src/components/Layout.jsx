cd104ba437ee415aac00ab94c73b2cfdc0275374
Pariharx7 <parihar.peace@gmail.com>
2026-04-24 18:04:58 +0530

    added styling for dark mode

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import ModeToggle from './ModeToggle';
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-gray-200 min-h-screen">
-      <div className="bg-white">
+    <div className="bg-gray-200 dark:bg-zinc-950 min-h-screen h-screen">
+      <div className="bg-background text-foreground min-h-screen flex flex-col">
         <Navbar />
-        <main>{children}</main> {/* This is where the page content will go */}
-        <div className="pt-0">
-          <Footer />
-        </div>
+        <main
+          className='flex-grow'
+        >{children}</main> {/* This is where the page content will go */}
+        {/* <div className='pt-0'> */}
+        <Footer />
+        {/* </div> */}
+        <ModeToggle />
       </div>
     </div>
   );
