9d633e58f67bdb80b89baaa753fd00917b24008d
Pariharx7 <parihar.peace@gmail.com>
2026-04-24 18:01:10 +0530

    added backend url via env for proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': "https://gemx-backend.vercel.app"   // modify this to backend link in production
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL, // modify this to backend link in production
+          changeOrigin: true,
+        },
+      },
     },
-  },
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+    plugins: [react(), tailwindcss()],
+  };
+});
