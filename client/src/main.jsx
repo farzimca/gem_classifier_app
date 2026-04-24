ec6c7f8bb52d2b6c51440a2e240abce89d901971
Pariharx7 <parihar.peace@gmail.com>
2026-04-24 17:58:42 +0530

    added ThemeProvider logic

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './store/auth.jsx'
+import { ThemeProvider } from 'next-themes'
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-  <StrictMode>
-    <App />
-  </StrictMode>,
-</AuthProvider>
+    <StrictMode>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <App />
+      </ThemeProvider>
+    </StrictMode>
+  </AuthProvider>
 )
