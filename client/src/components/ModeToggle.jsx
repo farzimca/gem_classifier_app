ccb369bfe27aa305d8391aabbe8834cd15884c42
Pariharx7 <parihar.peace@gmail.com>
2026-04-27 14:13:04 +0530

    Style changes for mobile screen

--- client/src/components/ModeToggle.jsx
+++ client/src/components/ModeToggle.jsx
@@ -38,7 +38,7 @@ const ModeToggle = () => {
 
     return (
         <button onClick={SWITCH_THEME}
-            className="absolute top-1 right-12 md:-right-2 md:top-1.4 mr-1 flex size-6 z-10 cursor-pointer items-center justify-center rounded-xl px-1 py-1 border bg-white/80 border-white/80 dark:border-neutral-800 dark:bg-background"
+            className="absolute top-3.5 right-12 md:-right-2 md:top-1  mr-1 flex size-6 z-10 cursor-pointer items-center justify-center rounded-xl px-1 py-1 border bg-white/80 border-white/80 dark:border-neutral-800 dark:bg-background"
         >
             <FaMoon className="absolute inset-0 m-auto size-5 shrink-0 scale-100 text-neutral-700 transition-all duration-200 dark:scale-0 dark:rotate-77" />
             <FaSun className="absolute inset-0 m-auto size-5 shrink-0 scale-0 rotate-77 text-white transition-all duration-200 dark:scale-100 dark:rotate-0 " />
